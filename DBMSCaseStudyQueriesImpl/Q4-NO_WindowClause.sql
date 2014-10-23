c328832a9c9358350c0a82b1b061762999732a53
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2014-10-23 20:37:02 +0100

    [SQLQueiesImpl] - Q4 implemented with NO WINDOW operator

--- DBMSCaseStudyQueriesImpl/Q4-NO_WindowClause.sql
+++ DBMSCaseStudyQueriesImpl/Q4-NO_WindowClause.sql
@@ -32,7 +32,9 @@ WHERE	rel1.location = rel2.location
 -----------------------------------------------------------
 -- Q4.1 GET last 10 minutes timestamps for each location --
 -----------------------------------------------------------
-SELECT 	rel1.measure_timestamp, measure, rel1.location
+/*--SELECT 	rel1.measure_timestamp, measure, rel1.location
+
+SELECT  rel1.location, avg(measure) AS measure_10minutes_avg
 FROM	"DBMS_EMS_Schema"."DenormalizedAggPhases" AS rel1,
 	(	SELECT location, MAX(measure_timestamp) AS max_ts
 		FROM "DBMS_EMS_Schema"."DenormalizedAggPhases" 
@@ -40,5 +42,31 @@ FROM	"DBMS_EMS_Schema"."DenormalizedAggPhases" AS rel1,
 	) AS rel2
 WHERE	rel1.location = rel2.location
     AND rel1.measure_timestamp >= rel2.max_ts - interval '10 minutes'
-ORDER BY measure_timestamp DESC
-	
+GROUP BY rel1.location
+
+--ORDER BY measure_timestamp DESC */
+
+
+SELECT  now_measure.measure_timestamp, now_measure.locatsion, ((now_measure.measure/min10avg_measure.measure_10minutes_avg) - 1) AS ratio
+FROM	(
+		SELECT 	rel1.measure_timestamp, measure, rel1.location
+		FROM	"DBMS_EMS_Schema"."DenormalizedAggPhases" AS rel1,
+		(	SELECT location, MAX(measure_timestamp) AS max_ts
+			FROM "DBMS_EMS_Schema"."DenormalizedAggPhases" 
+			GROUP BY location
+		) AS rel2
+		WHERE	rel1.location = rel2.location
+		AND rel1.measure_timestamp = rel2.max_ts
+	) AS now_measure,
+	(
+		SELECT  rel1.location, avg(measure) AS measure_10minutes_avg
+		FROM	"DBMS_EMS_Schema"."DenormalizedAggPhases" AS rel1,
+			(	SELECT location, MAX(measure_timestamp) AS max_ts
+				FROM "DBMS_EMS_Schema"."DenormalizedAggPhases" 
+				GROUP BY location
+			) AS rel2
+		WHERE	rel1.location = rel2.location
+		AND rel1.measure_timestamp >= rel2.max_ts - interval '10 minutes'
+		GROUP BY rel1.location
+	) AS min10avg_measure
+WHERE 	now_measure.location = min10avg_measure.location
