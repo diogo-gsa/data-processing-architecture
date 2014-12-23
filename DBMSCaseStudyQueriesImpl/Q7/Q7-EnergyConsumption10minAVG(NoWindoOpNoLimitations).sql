85af38bab3901e276ddd4ae47a65749c0c780684
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2014-12-23 18:12:37 +0000

    [ADDing] Redifinig Q7 (in a "branch" query implementation, Q78 was not modified) due the Q3 Implementation

---
+++ DBMSCaseStudyQueriesImpl/Q7/Q7-EnergyConsumption10minAVG(NoWindoOpNoLimitations).sql
@@ -0,0 +1,13 @@
+﻿SELECT 	R1.device_pk,
+	R1.measure_timestamp,
+	R1.measure,
+	R2.device_pk,
+	R2.measure_timestamp,
+	R2.measure
+FROM 	"DBMS_EMS_Schema"."DenormalizedAggPhases" AS R1
+	INNER JOIN
+	"DBMS_EMS_Schema"."DenormalizedAggPhases" AS R2
+	ON R1.device_pk = R2.device_pk
+	  AND R2.measure_timestamp >= R1.measure_timestamp - interval '3 minutes'
+ORDER BY R1.device_pk ASC, R1.measure_timestamp DESC, R2.measure_timestamp DESC
+	 
