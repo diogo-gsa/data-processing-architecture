dbe106e35d0b4b19d9852cb07143ea31cb57dcda
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2015-01-17 22:30:16 +0000

    [ADDing - ] Q13 (DBMS) is under implementation.

--- DBMSCaseStudyQueriesImpl/Q13/Q13.sql
+++ DBMSCaseStudyQueriesImpl/Q13/Q13.sql
@@ -1,14 +1,42 @@
-﻿SELECT device_pk,
-	date_part('hour',measure_timestamp),
-	avg(measure_avg_10min),
-	sum(measure_avg_10min),
-	count(measure_avg_10min)
-	--,avg(measure_avg_10min) 	OVER w AS expected,
-	--rank() 			OVER w
-FROM "DBMS_EMS_Schema"."Q7_10minAVG"
---WINDOW w AS (PARTITION BY device_pk
---	      ORDER BY measure_timestamp DESC
---	      RANGE BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
-GROUP BY device_pk, date_part('hour',measure_timestamp)
-ORDER BY device_pk, date_part('hour',measure_timestamp)
---ORDER BY device_pk, measure_timestamp
+﻿SELECT 	all_measures.device_pk,
+	date_part('hour', all_measures.measure_timestamp),
+	avg(all_measures.measure_avg_10min),
+	sum(all_measures.measure_avg_10min),
+	count(all_measures.measure_avg_10min)
+	
+FROM 	"DBMS_EMS_Schema"."Q7_10minAVG"   		AS all_measures
+	INNER JOIN 
+	(SELECT device_pk, 
+		max(measure_timestamp) AS current_ts
+	FROM 	"DBMS_EMS_Schema"."Q7_10minAVG"
+	GROUP BY device_pk) 		   		AS most_recent_measure
+	ON  most_recent_measure.device_pk = all_measures.device_pk	
+	AND all_measures.measure_timestamp >=  most_recent_measure.current_ts  - interval '3 day'
+	
+GROUP BY all_measures.device_pk, date_part('hour', all_measures.measure_timestamp)
+
+ORDER BY all_measures.device_pk, date_part('hour', all_measures.measure_timestamp)
+
+
+
+
+/*
+SELECT 	all_measures.device_pk,
+	date_part('hour', all_measures.measure_timestamp),
+	avg(all_measures.measure_avg_10min),
+	sum(all_measures.measure_avg_10min),
+	count(all_measures.measure_avg_10min)
+	
+FROM 	"DBMS_EMS_Schema"."Q7_10minAVG"   		AS all_measures
+	INNER JOIN 
+	(SELECT device_pk, 
+		max(measure_timestamp) AS current_ts
+	FROM 	"DBMS_EMS_Schema"."Q7_10minAVG"
+	GROUP BY device_pk) 		   		AS most_recent_measure
+	ON  most_recent_measure.device_pk = all_measures.device_pk	
+	AND all_measures.measure_timestamp >=  most_recent_measure.current_ts  - interval '1 day'
+	
+GROUP BY all_measures.device_pk, date_part('hour', all_measures.measure_timestamp)
+
+ORDER BY all_measures.device_pk, date_part('hour', all_measures.measure_timestamp)
+*/
