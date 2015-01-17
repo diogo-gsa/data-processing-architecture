2c2be7d5a8badc2dafe0abcba2ed43d371e3c017
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2015-01-17 23:18:23 +0000

    [ADDing - ] Q13 (DBMS) is under implementation.

--- DBMSCaseStudyQueriesImpl/Q13/Q13.sql
+++ DBMSCaseStudyQueriesImpl/Q13/Q13.sql
@@ -1,8 +1,12 @@
-﻿SELECT 	all_measures.device_pk,
+﻿SELECT *
+FROM(
+SELECT 	all_measures.device_pk,
+	all_measures.measure_timestamp,
 	date_part('hour', all_measures.measure_timestamp),
-	avg(all_measures.measure_avg_10min),
-	sum(all_measures.measure_avg_10min),
-	count(all_measures.measure_avg_10min)
+	avg(all_measures.measure_avg_10min) 			OVER w,
+	sum(all_measures.measure_avg_10min) 			OVER w,
+	count(all_measures.measure_avg_10min) 			OVER w,
+	rank()							OVER w
 	
 FROM 	"DBMS_EMS_Schema"."Q7_10minAVG"   		AS all_measures
 	INNER JOIN 
@@ -12,10 +16,14 @@ FROM 	"DBMS_EMS_Schema"."Q7_10minAVG"   		AS all_measures
 	GROUP BY device_pk) 		   		AS most_recent_measure
 	ON  most_recent_measure.device_pk = all_measures.device_pk	
 	AND all_measures.measure_timestamp >=  most_recent_measure.current_ts  - interval '3 day'
-	
-GROUP BY all_measures.device_pk, date_part('hour', all_measures.measure_timestamp)
 
-ORDER BY all_measures.device_pk, date_part('hour', all_measures.measure_timestamp)
+WINDOW w AS (PARTITION BY all_measures.device_pk, date_part('hour', all_measures.measure_timestamp)
+	     ORDER BY all_measures.measure_timestamp DESC
+	     RANGE BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
+) AS rel
+WHERE rel.rank = 1
+--GROUP BY all_measures.device_pk, date_part('hour', all_measures.measure_timestamp)
+--ORDER BY all_measures.device_pk, date_part('hour', all_measures.measure_timestamp)
 
 
 
