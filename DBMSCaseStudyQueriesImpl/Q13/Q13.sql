9b7fbe1fdf8860bd60349d41e095228031ea90c0
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2015-01-17 20:42:10 +0000

    [ADDing - ] Q13 (DBMS) is under implementation.

---
+++ DBMSCaseStudyQueriesImpl/Q13/Q13.sql
@@ -0,0 +1,14 @@
+﻿SELECT device_pk,
+	date_part('hour',measure_timestamp),
+	avg(measure_avg_10min),
+	sum(measure_avg_10min),
+	count(measure_avg_10min)
+	--,avg(measure_avg_10min) 	OVER w AS expected,
+	--rank() 			OVER w
+FROM "DBMS_EMS_Schema"."Q7_10minAVG"
+--WINDOW w AS (PARTITION BY device_pk
+--	      ORDER BY measure_timestamp DESC
+--	      RANGE BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
+GROUP BY device_pk, date_part('hour',measure_timestamp)
+ORDER BY device_pk, date_part('hour',measure_timestamp)
+--ORDER BY device_pk, measure_timestamp
