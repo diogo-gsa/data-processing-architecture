68e9bdb291211f1cf78b2810638947744cb2d9b5
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2015-01-18 12:56:43 +0000

    [ADDing - ] Q13 (DBMS) is under implementation.

--- DBMSCaseStudyQueriesImpl/Q13/Q13.sql
+++ DBMSCaseStudyQueriesImpl/Q13/Q13.sql
@@ -1,6 +1,14 @@
-﻿SELECT *
+﻿SELECT 	cluster_rel.device_pk,
+	cluster_rel.measure_timestamp,
+	all_measures.measure_avg_10min,
+	cluster_rel.cluster_timestamp,
+	cluster_rel.date_part,
+	cluster_rel.avg,
+	cluster_rel.sum,
+	cluster_rel.count,
+	cluster_rel.rank
 FROM	(SELECT	all_measures.device_pk,
-		most_recent_measure.current_ts				AS measure_timestamp,
+		most_recent_measure.ts					AS measure_timestamp,
 		all_measures.measure_timestamp				AS cluster_timestamp,
 		date_part('hour', all_measures.measure_timestamp),
 		avg(all_measures.measure_avg_10min) 			OVER w,
@@ -11,39 +19,47 @@ FROM	(SELECT	all_measures.device_pk,
 	FROM 	"DBMS_EMS_Schema"."Q7_10minAVG"   		AS all_measures
 		INNER JOIN 
 		(SELECT device_pk, 
-			max(measure_timestamp) 	AS current_ts
+			max(measure_timestamp) 	AS ts
 		FROM 	"DBMS_EMS_Schema"."Q7_10minAVG"
 		GROUP BY device_pk) 		   		AS most_recent_measure
 	
 	ON  most_recent_measure.device_pk = all_measures.device_pk	
-	AND all_measures.measure_timestamp >=  most_recent_measure.current_ts  - interval '3 day'
+	AND all_measures.measure_timestamp >=  most_recent_measure.ts  - interval '3 day'
 
 	WINDOW w AS (PARTITION BY all_measures.device_pk, date_part('hour', all_measures.measure_timestamp)
 		 ORDER BY all_measures.measure_timestamp DESC
 		 RANGE BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
-	) AS rel
-WHERE rel.rank = 1
-
-
-
-
-/*
-SELECT 	all_measures.device_pk,
-	date_part('hour', all_measures.measure_timestamp),
-	avg(all_measures.measure_avg_10min),
-	sum(all_measures.measure_avg_10min),
-	count(all_measures.measure_avg_10min)
-	
-FROM 	"DBMS_EMS_Schema"."Q7_10minAVG"   		AS all_measures
+	) 				AS cluster_rel
 	INNER JOIN 
-	(SELECT device_pk, 
-		max(measure_timestamp) AS current_ts
-	FROM 	"DBMS_EMS_Schema"."Q7_10minAVG"
-	GROUP BY device_pk) 		   		AS most_recent_measure
-	ON  most_recent_measure.device_pk = all_measures.device_pk	
-	AND all_measures.measure_timestamp >=  most_recent_measure.current_ts  - interval '1 day'
-	
-GROUP BY all_measures.device_pk, date_part('hour', all_measures.measure_timestamp)
+	"DBMS_EMS_Schema"."Q7_10minAVG"	AS all_measures
+	ON  cluster_rel.rank = 1 
+	AND all_measures.device_pk = cluster_rel.device_pk
+	AND all_measures.measure_timestamp = cluster_rel.measure_timestamp	
+	AND date_part('hour', all_measures.measure_timestamp) = cluster_rel.date_part
 
-ORDER BY all_measures.device_pk, date_part('hour', all_measures.measure_timestamp)
-*/
+
+/*SELECT *
+FROM	(SELECT	all_measures.device_pk,
+		most_recent_measure.ts					AS measure_timestamp,
+		all_measures.measure_timestamp				AS cluster_timestamp,
+		date_part('hour', all_measures.measure_timestamp),
+		avg(all_measures.measure_avg_10min) 			OVER w,
+		sum(all_measures.measure_avg_10min) 			OVER w,
+		count(all_measures.measure_avg_10min) 			OVER w,
+		rank()							OVER w
+	
+	FROM 	"DBMS_EMS_Schema"."Q7_10minAVG"   		AS all_measures
+		INNER JOIN 
+		(SELECT device_pk, 
+			max(measure_timestamp) 	AS ts
+		FROM 	"DBMS_EMS_Schema"."Q7_10minAVG"
+		GROUP BY device_pk) 		   		AS most_recent_measure
+	
+	ON  most_recent_measure.device_pk = all_measures.device_pk	
+	AND all_measures.measure_timestamp >=  most_recent_measure.ts  - interval '3 day'
+
+	WINDOW w AS (PARTITION BY all_measures.device_pk, date_part('hour', all_measures.measure_timestamp)
+		 ORDER BY all_measures.measure_timestamp DESC
+		 RANGE BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
+	) AS cluster_rel
+WHERE cluster_rel.rank = 1*/
