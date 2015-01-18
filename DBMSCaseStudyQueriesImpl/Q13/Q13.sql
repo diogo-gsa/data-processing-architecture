c11fc38843f404c4adc7d9d92631ff34368c40f9
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2015-01-18 12:10:37 +0000

    [ADDing - ] Q13 (DBMS) is under implementation.

--- DBMSCaseStudyQueriesImpl/Q13/Q13.sql
+++ DBMSCaseStudyQueriesImpl/Q13/Q13.sql
@@ -1,29 +1,28 @@
 ﻿SELECT *
-FROM(
-SELECT 	all_measures.device_pk,
-	all_measures.measure_timestamp,
-	date_part('hour', all_measures.measure_timestamp),
-	avg(all_measures.measure_avg_10min) 			OVER w,
-	sum(all_measures.measure_avg_10min) 			OVER w,
-	count(all_measures.measure_avg_10min) 			OVER w,
-	rank()							OVER w
+FROM	(SELECT	all_measures.device_pk,
+		most_recent_measure.current_ts				AS measure_timestamp,
+		all_measures.measure_timestamp				AS cluster_timestamp,
+		date_part('hour', all_measures.measure_timestamp),
+		avg(all_measures.measure_avg_10min) 			OVER w,
+		sum(all_measures.measure_avg_10min) 			OVER w,
+		count(all_measures.measure_avg_10min) 			OVER w,
+		rank()							OVER w
 	
-FROM 	"DBMS_EMS_Schema"."Q7_10minAVG"   		AS all_measures
-	INNER JOIN 
-	(SELECT device_pk, 
-		max(measure_timestamp) AS current_ts
-	FROM 	"DBMS_EMS_Schema"."Q7_10minAVG"
-	GROUP BY device_pk) 		   		AS most_recent_measure
+	FROM 	"DBMS_EMS_Schema"."Q7_10minAVG"   		AS all_measures
+		INNER JOIN 
+		(SELECT device_pk, 
+			max(measure_timestamp) 	AS current_ts
+		FROM 	"DBMS_EMS_Schema"."Q7_10minAVG"
+		GROUP BY device_pk) 		   		AS most_recent_measure
+	
 	ON  most_recent_measure.device_pk = all_measures.device_pk	
 	AND all_measures.measure_timestamp >=  most_recent_measure.current_ts  - interval '3 day'
 
-WINDOW w AS (PARTITION BY all_measures.device_pk, date_part('hour', all_measures.measure_timestamp)
-	     ORDER BY all_measures.measure_timestamp DESC
-	     RANGE BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
-) AS rel
+	WINDOW w AS (PARTITION BY all_measures.device_pk, date_part('hour', all_measures.measure_timestamp)
+		 ORDER BY all_measures.measure_timestamp DESC
+		 RANGE BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
+	) AS rel
 WHERE rel.rank = 1
---GROUP BY all_measures.device_pk, date_part('hour', all_measures.measure_timestamp)
---ORDER BY all_measures.device_pk, date_part('hour', all_measures.measure_timestamp)
 
 
 
