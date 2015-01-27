380eec1f4869ad690ce9db62a9ca0adfd907cb2e
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2015-01-27 23:10:33 +0000

    [FIX] - Q13 DBMS (HistoricVersion) bug solved? I think so, but we have to double check with some intensive tests

--- DBMSCaseStudyQueriesImpl/NEW_Q13_DeltaBetweenCurrentConsumptionAndLastMonthBasedPrediction/NEW_Q13_HistoricVersion.sql
+++ DBMSCaseStudyQueriesImpl/NEW_Q13_DeltaBetweenCurrentConsumptionAndLastMonthBasedPrediction/NEW_Q13_HistoricVersion.sql
@@ -1,5 +1,5 @@
 ﻿SELECT	device_pk,
-	measure_timestamp,
+	measure_timestamp_pivot, -- ALTEREI ISTO 
 	measure,
 	expecetd_measure,
 	measure - expecetd_measure AS delta,
@@ -9,7 +9,8 @@
 	rank()	OVER w2
 	
 FROM	(SELECT	pivot_measures.device_pk,
-		pivot_measures.measure_timestamp		AS measure_timestamp,
+		pivot_measures.measure_timestamp		AS measure_timestamp_pivot,   -- ALTEREI ISTO 
+		cluster_measures.measure_timestamp		AS measure_timestamp_cluster, -- ALTEREI ISTO
 		pivot_measures.measure 				AS measure,
 		avg(cluster_measures.measure)	     OVER w1	AS expecetd_measure,
 		pivot_measures.measure_description,
@@ -31,7 +32,8 @@ FROM	(SELECT	pivot_measures.device_pk,
 				RANGE BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
 	) AS rel 	
 
-WHERE rel.rank = 1
+WHERE rel.rank = 1 
+	AND date_part('hour', measure_timestamp_pivot) = date_part('hour', measure_timestamp_cluster) -- ALTEREI ISTO 
 
 WINDOW 	w2 AS 	(PARTITION BY 	device_pk
-		 ORDER BY	measure_timestamp DESC)
+		 ORDER BY	measure_timestamp_pivot DESC) -- ALTEREI ISTO 
