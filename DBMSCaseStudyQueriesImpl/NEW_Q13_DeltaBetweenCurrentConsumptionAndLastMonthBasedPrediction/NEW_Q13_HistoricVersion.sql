8293256425ce31a7b66a8f0746be1776e978fe08
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2015-01-27 15:58:12 +0000

    [REF][ADD] - New Q13 DBMS Implemented and Tested by not yet deployed at DBMS server

--- DBMSCaseStudyQueriesImpl/NEW_Q13_DeltaBetweenCurrentConsumptionAndLastMonthBasedPrediction/NEW_Q13_HistoricVersion.sql
+++ DBMSCaseStudyQueriesImpl/NEW_Q13_DeltaBetweenCurrentConsumptionAndLastMonthBasedPrediction/NEW_Q13_HistoricVersion.sql
@@ -1,40 +1,36 @@
-﻿--Version: Histoirc
-SELECT 	all_measures.device_pk,
-	all_measures.measure_timestamp,
-	all_measures.measure						AS measure,
-	clustered_measures.cluster_avg_measure					AS cluster_expected_measure,
-	all_measures.measure - clustered_measures.cluster_avg_measure	AS delta,
-	clustered_measures.cluster_pivot_hour,					
-	clustered_measures.cluster_timestamp					AS cluster_last_update,
-	clustered_measures.cluster_dataset_size/60				AS cluster_dataset_days,
+﻿SELECT	device_pk,
+	measure_timestamp,
+	measure,
+	expecetd_measure,
+	measure_description,
 	measure_unit,
-	'Delta between current and last month avg consumption '			AS measure_description,
 	device_location,
-	clustered_measures.rank -- DEBUG (ou talvez não, caso fique a versão Historic)
+	rank()	OVER w2
 	
-FROM	(SELECT	all_measures.device_pk,
-		most_recent_measure.measure_timestamp		  AS measure_timestamp,
-		all_measures.measure_timestamp		          AS cluster_timestamp,
-		date_part('hour', all_measures.measure_timestamp) AS cluster_pivot_hour,
-		avg(all_measures.measure) 	OVER w	  AS cluster_avg_measure,
-		count(all_measures.measure)	OVER w	  AS cluster_dataset_size,
-		rank()				OVER w
-	
-	FROM 	"DBMS_EMS_Schema"."New_Q8_NormalizeConsumptionsByLocationSquareMeters"   	AS all_measures
+FROM	(SELECT	pivot_measures.device_pk,
+		pivot_measures.measure_timestamp		AS measure_timestamp,
+		pivot_measures.measure 				AS measure,
+		avg(cluster_measures.measure)	     OVER w1	AS expecetd_measure,
+		pivot_measures.measure_description,
+		pivot_measures.measure_unit,
+		pivot_measures.device_location,
+		rank()				     OVER w1
+		
+	FROM 	"DBMS_EMS_Schema"."New_Q8_NormalizeConsumptionsByLocationSquareMeters"   	AS cluster_measures
 		INNER JOIN 
-		"DBMS_EMS_Schema"."New_Q8_NormalizeConsumptionsByLocationSquareMeters"		AS most_recent_measure
-		ON  /*most_recent_measure.rank 	    = 1
-		AND*/ most_recent_measure.device_pk   = all_measures.device_pk	
-		AND all_measures.measure_timestamp >= most_recent_measure.measure_timestamp  - interval '3 minutes'
+		"DBMS_EMS_Schema"."New_Q8_NormalizeConsumptionsByLocationSquareMeters"		AS pivot_measures
+		ON  cluster_measures.device_pk		= pivot_measures.device_pk
+		AND cluster_measures.measure_timestamp <= pivot_measures.measure_timestamp
+		AND cluster_measures.measure_timestamp  > pivot_measures.measure_timestamp  - interval '3 minutes' -- Must Change to 3 months
+				
+		WINDOW 	w1 AS 	(PARTITION BY 	cluster_measures.device_pk, 
+						date_part('hour', cluster_measures.measure_timestamp),
+				   		pivot_measures.rank
+				ORDER BY	cluster_measures.measure_timestamp DESC
+				RANGE BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
+	) AS rel 	
 
-	WINDOW 	w AS (PARTITION BY all_measures.device_pk, 	
-				   date_part('hour', all_measures.measure_timestamp)
-		      ORDER BY all_measures.measure_timestamp DESC
-	              RANGE BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
-	) AS clustered_measures
-	INNER JOIN 
-	"DBMS_EMS_Schema"."New_Q8_NormalizeConsumptionsByLocationSquareMeters"	AS all_measures
-	ON  /*clustered_measures.rank = 1 
-	AND*/ all_measures.device_pk = clustered_measures.device_pk
-	AND all_measures.measure_timestamp = clustered_measures.measure_timestamp	
-	AND date_part('hour', all_measures.measure_timestamp) = clustered_measures.cluster_pivot_hour
+WHERE rel.rank = 1
+
+WINDOW 	w2 AS 	(PARTITION BY 	device_pk
+		 ORDER BY	measure_timestamp DESC)
