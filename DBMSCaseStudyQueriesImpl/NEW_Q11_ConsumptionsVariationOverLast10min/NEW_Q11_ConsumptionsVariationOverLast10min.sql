3b74ec9487a1b69b1733e10aa7023bfa0fff8d15
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2015-01-31 14:55:56 +0000

    [REF][ADD] - New Q11  (DBMS) Implemented and Tested

---
+++ DBMSCaseStudyQueriesImpl/NEW_Q11_ConsumptionsVariationOverLast10min/NEW_Q11_ConsumptionsVariationOverLast10min.sql
@@ -0,0 +1,24 @@
+﻿-- Q11_WindowWorkaround_10min ###############
+
+SELECT dga.device_pk,
+	dga.device_location,
+	dga.measure_timestamp,	
+	(dga.measure/(avg(dga.measure) 	OVER w +0.00001) - 1) 	AS variation,	
+	dga.measure 						AS current_measure,
+	avg(dga.measure) 		OVER w 			AS win_measure,
+	rank() 		 		OVER w 			AS rank
+	
+FROM 	"DBMS_EMS_Schema"."DenormalizedAggPhases" 	AS dga,
+	(SELECT "DenormalizedAggPhases".device_pk,
+		max("DenormalizedAggPhases".measure_timestamp) AS ts
+	FROM "DBMS_EMS_Schema"."DenormalizedAggPhases"
+	GROUP BY "DenormalizedAggPhases".device_pk) 	AS dga_last_ts
+	
+WHERE dga.device_pk = dga_last_ts.device_pk 
+	AND dga.measure_timestamp > (dga_last_ts.ts - '00:05:00'::interval)
+	
+WINDOW w AS 	(PARTITION BY dga.device_pk 	
+		ORDER BY dga.measure_timestamp 
+		DESC RANGE BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
+
+--WHERE rel.rank = 1;	
