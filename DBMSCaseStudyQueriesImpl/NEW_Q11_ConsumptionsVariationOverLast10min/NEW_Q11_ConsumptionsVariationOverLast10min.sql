2da084fdcddf77772cda54ab73591b12888a586e
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2015-02-02 13:48:50 +0000

    [REF][Fix] - New Q11  (DBMS) Fixed

--- DBMSCaseStudyQueriesImpl/NEW_Q11_ConsumptionsVariationOverLast10min/NEW_Q11_ConsumptionsVariationOverLast10min.sql
+++ DBMSCaseStudyQueriesImpl/NEW_Q11_ConsumptionsVariationOverLast10min/NEW_Q11_ConsumptionsVariationOverLast10min.sql
@@ -1,20 +1,30 @@
-﻿SELECT 	dga.device_pk,
-	dga.measure_timestamp,	
-	(dga.measure/(avg(dga.measure) 	OVER w +0.00001) - 1) 	AS variation,	
-	dga.measure 						AS current_measure,
-	avg(dga.measure) 		OVER w 			AS win_measure,
-	dga.device_location,
-	rank() 		 		OVER w 			AS rank
+﻿-- New Q11
+SELECT  r1.device_pk,
+	r1.measure_timestamp,
+	((r1.measure/(avg(r2.measure)+0.00001))-1)	AS variation,	
+	r1.measure 					AS current_measure,
+	avg(r2.measure) 				AS win_measure,
+	r1.measure_unit::text,
+	r1.measure_description::text,
+	r1.device_location,
+	r1.location_area_m2,
+	rank()	OVER w
 	
-FROM 	"DBMS_EMS_Schema"."DenormalizedAggPhases" 	AS dga,
-	(SELECT "DenormalizedAggPhases".device_pk,
-		max("DenormalizedAggPhases".measure_timestamp) AS ts
-	FROM "DBMS_EMS_Schema"."DenormalizedAggPhases"
-	GROUP BY "DenormalizedAggPhases".device_pk) 	AS dga_last_ts
-	
-WHERE dga.device_pk = dga_last_ts.device_pk 
-	AND dga.measure_timestamp > (dga_last_ts.ts - '00:05:00'::interval)
-	
-WINDOW w AS 	(PARTITION BY dga.device_pk 	
-		ORDER BY dga.measure_timestamp 
-		DESC RANGE BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
+FROM 	"DBMS_EMS_Schema"."DenormalizedAggPhases" 	AS r1
+	INNER JOIN
+	"DBMS_EMS_Schema"."DenormalizedAggPhases" 	AS r2
+	ON  r1.device_pk = r2.device_pk 
+	AND r2.measure_timestamp > (r1.measure_timestamp - '00:05:00'::interval) 
+	AND r2.measure_timestamp <= r1.measure_timestamp
+
+GROUP BY r1.device_pk, 
+	 r1.measure_timestamp, 
+	 r1.measure, 
+	 r1.measure_unit::text, 
+	 r1.measure_description::text, 
+	 r1.device_location, 
+	 r1.location_area_m2
+		
+WINDOW w AS (PARTITION BY r1.device_pk 	
+	     ORDER BY r1.measure_timestamp 
+	     DESC RANGE BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
