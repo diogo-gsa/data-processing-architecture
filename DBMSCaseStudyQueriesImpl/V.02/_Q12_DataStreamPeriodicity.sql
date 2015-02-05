8689bc52e03aaa38d9f578deea7007a151dc0955
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2015-02-05 19:09:58 +0000

    [REF2][ADD] - Q12 at DBMS Implemented, Tested, and Deployed

---
+++ DBMSCaseStudyQueriesImpl/V.02/_Q12_DataStreamPeriodicity.sql
@@ -0,0 +1,14 @@
+﻿SELECT  rela.device_pk,
+        rela.measure_timestamp                                    AS measure_timestamp,
+        rela.measure_timestamp - relb.measure_timestamp           AS measure,
+        'Time Seconds'                                            AS measure_unit,
+        'Period between two last power consumption measurements.' AS measure_description,
+        rela.device_location,
+        rela.location_area_m2,
+        rela.index
+
+FROM    "DBMS_EMS_Schema"."_Q00_DataAggregation" rela
+        JOIN 
+        "DBMS_EMS_Schema"."_Q00_DataAggregation" relb 
+        ON rela.device_pk = relb.device_pk 
+        AND (rela.index + 1) = relb.index
