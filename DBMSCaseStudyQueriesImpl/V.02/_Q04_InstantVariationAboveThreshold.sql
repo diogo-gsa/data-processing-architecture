b3bf4c1c6f7f68c49ed3d3cf154f24da8c28d823
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2015-02-05 18:43:39 +0000

    [REF2][ADD] - Q0,Q11,Q4 at DBMS Implemented, Tested, and Deployed

---
+++ DBMSCaseStudyQueriesImpl/V.02/_Q04_InstantVariationAboveThreshold.sql
@@ -0,0 +1,20 @@
+﻿SELECT  device_pk,
+	measure_timestamp,
+	measure,
+	current_power_consumption,
+	'Percentage%' 												  AS measure_unit,
+	'Variation between current and last 5 minutes average power consumption that exceeded a given threshold.' AS measure_description,
+	device_location,
+	location_area_m2
+
+FROM "DBMS_EMS_Schema"."_Q11_InstantVariation"
+
+WHERE index = 1
+AND ((device_pk = 1 AND measure >= -100)
+  OR (device_pk = 2 AND measure >= -100)
+  OR (device_pk = 3 AND measure >= -100)
+  OR (device_pk = 4 AND measure >= -100)
+  OR (device_pk = 5 AND measure >= -100)
+  OR (device_pk = 6 AND measure >= -100)
+  OR (device_pk = 7 AND measure >= -100)
+  OR (device_pk = 8 AND measure >= -100))
--- src/msc_thesis/diogo_anjos/DBMS_Version/DB_CRUD_Query_API.java
+++ src/msc_thesis/diogo_anjos/DBMS_Version/DB_CRUD_Query_API.java
@@ -138,26 +138,29 @@ public class DB_CRUD_Query_API {
 //								Case Study Queries Implementation 
 //	==========================================================================================
 	
-	public QueryEvaluationReport executeEvaluationQuery_New_Q4_VariationsAboveThreshold(){
-		String queryStatement =	"SELECT device_pk, "														+ 
-										"measure_timestamp, " 												+
-										"variation, " 														+
-										"current_measure, " 												+ 
-										"win_measure, " 													+
-										"device_location, " 												+
-										"rank " 															+
-								"FROM \"DBMS_EMS_Schema\".\"New_Q11_ConsumptionsVariationOverLast5min\" " 	+
-								"WHERE rank = 1 " +
-									"AND ((device_pk = 1 AND variation >= -100) " +  									
-									  "OR (device_pk = 2 AND variation >= -100) "	+								
-									  "OR (device_pk = 3 AND variation >= -100) "	+								
-									  "OR (device_pk = 4 AND variation >= -100) "	+								
-									  "OR (device_pk = 5 AND variation >= -100) "	+								
-									  "OR (device_pk = 6 AND variation >= -100) "	+								
-									  "OR (device_pk = 7 AND variation >= -100) "	+								
-									  "OR (device_pk = 8 AND variation >= -100)) ";
-								//IMPORTANT: Use device_pk = X AND variation >= -1000 for universal condition
-		
+	// _Q04_InstantVariationAboveThreshold
+	public QueryEvaluationReport execute_Q04_InstantVariationAboveThreshold(){
+		String queryStatement =	"SELECT  device_pk, " 																					+
+										"measure_timestamp, " 																			+
+										"measure, " 																					+
+										"current_power_consumption, " 																	+
+										"'Percentage%' 														AS measure_unit, " 			+
+										"'Variation between current and last 5 minutes average power " 									+
+											"consumption that exceeded a given threshold.' 					AS measure_description, " 	+
+										"device_location, " 																			+
+										"location_area_m2 " 																			+
+								"FROM \"DBMS_EMS_Schema\".\"_Q11_InstantVariation\" " 													+
+								"WHERE index = 1 " 																						+
+								  "AND ((device_pk = 1 AND measure >= -100) " 															+
+								    "OR (device_pk = 2 AND measure >= -100) " 															+
+								    "OR (device_pk = 3 AND measure >= -100) " 															+
+								    "OR (device_pk = 4 AND measure >= -100) " 															+
+								    "OR (device_pk = 5 AND measure >= -100) " 															+
+								    "OR (device_pk = 6 AND measure >= -100) " 															+		 	
+								    "OR (device_pk = 7 AND measure >= -100) " 															+
+								    "OR (device_pk = 8 AND measure >= -100)) ";
+							//IMPORTANT: Use device_pk = X AND variation >= -1000 for universal condition
+
 		return executeEvaluationQuery(queryStatement);	
 	}
 	
