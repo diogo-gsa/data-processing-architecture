9b3cfd3bf98b44227a49ed03ff544a9815fe951d
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2015-02-06 13:14:25 +0000

    [REF2][ADD] - Q14 at DBMS Implemented, Tested, and Deployed

---
+++ DBMSCaseStudyQueriesImpl/V.02/_Q14_ExpectedConsumptionByUDF.sql
@@ -0,0 +1,8 @@
+﻿SELECT  device_pk,
+        measure_timestamp,
+        measure 									  AS current_measure,
+        "DBMS_EMS_Schema".get_expected_measure_udf(device_pk::integer, measure_timestamp) AS expected_measure,
+        'WATT/m^2'									  AS measure_unit,
+        'Current and Expected Power consumption given by a User Defined Function (UDF).'  AS measure_description,
+        device_location
+FROM    "DBMS_EMS_Schema"."_Q08_SquareMeterNormalization"
